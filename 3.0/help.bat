4682d56b0410470d769f4084e51fd520d4d9552b
Mihot7 <105102302+Mihot7@users.noreply.github.com>
2024-05-12 15:27:43 +0200

    Added support for 3.0

---
+++ 3.0/help.bat
@@ -0,0 +1,6 @@
+echo Lista zainstalowanych Programów:
+for %%a in (*.bat) do @echo %%~na
+echo.
+echo.
+set prog=1
+%shell%
---
+++ 3.0/music.bat
@@ -0,0 +1,73 @@
+cd ..
+cd system
+chcp 65001
+
+:poczatek
+cls
+echo MIHOT MUSIC       │
+echo ──────────────────┘
+echo.
+echo 1) Odtwórz plik
+echo 2) Zatrzymaj muzyke
+echo 3) Info
+echo 4) KONIEC
+echo.
+set /p opcja=wybierz:
+if %opcja%==1 goto play
+if %opcja%==2 goto stop
+if %opcja%==3 goto Info
+if %opcja%==4 goto exit
+goto zly_wybor
+
+:zly_wybor
+echo OPCJA NIE ZNANA.. WYBIERZ PONOWNIE!
+pause
+goto poczatek
+
+:play
+cls
+rem     │     ─   └    ┘    ┌    ┐    ┤    ├    ┼    ┴    ┬
+echo MIHOT MUSIC       │
+echo ──────────────────┘
+echo.
+echo.
+echo.
+echo Wybierz plik którego chcesz odsłuchać:
+cd ..
+cd user_files
+echo.
+for %%a in (*.wav) do @echo %%~na
+echo.
+cd ..
+cd system
+echo Nie widzisz swojego pliku? Pamiętaj, przekonwertuj go zapomocą polecenia convert!
+set /p x=Nazwa: 
+echo.
+cls
+type playing.txt
+start silent.bat PlaySound.bat "%userfolder%\%x%.wav"
+pause
+goto poczatek
+
+:stop
+cls
+taskkill /f /im wscript.exe
+cls
+type stop.txt
+pause
+goto poczatek
+
+:info
+cls
+echo Mihot MUSIC v2.1
+echo 2021-2024 Mihot7 i NotAdidel
+echo Using NotAdidel's PlaySound lib.
+echo Original version made by NotAdidel for Mihot 5.0
+pause
+goto poczatek
+
+:exit
+cd ..
+cd prog
+set prog=1
+%shell%
