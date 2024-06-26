3cf6aa1ba0b091cdf6666bc0f5d02c56de1ed74b
Mihot7 <105102302+Mihot7@users.noreply.github.com>
2024-06-26 10:19:05 +0200

    Added support for 4.0

---
+++ 4.0/repo.bat
@@ -0,0 +1,28 @@
+echo ───────────────────────────┐
+echo Godzina: %time%
+echo ──────────────────┬────────┘
+echo MTOS REPO         │
+echo ──────────────────┘
+cho Witaj w repo!
+Echo Sprawdzanie połączenia internetowego...
+Ping www.google.com -n 1 -w 1000
+cls
+if errorlevel 1 (echo Brak połączenia internetowego! && goto exit) else (echo Jesteś połączony z internetem! && goto repo)
+
+:repo
+echo Podaj nazwe programu który chcesz zainstalować! (Jeśli program sie nie zainstaluje to znaczy że, nie INSTNIEJE)
+set /p pkg=Nazwa:
+if exist %pkg%.bat (
+    echo PAKIET INSTNIEJE! && goto exit
+) else (
+    goto install
+)
+
+:install
+curl https://raw.githubusercontent.com/Mihot7/MTOS-REPO/main/%pkg%.bat --silent --output %pkg%.bat
+Echo Pakiet: %pkg% ZAINSTALOWANY! (?)
+goto exit
+
+:exit
+set prog=1
+%shell%
